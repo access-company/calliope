ce9634dd6d721e074a5a172c7bde673946b8345d
Yuki Sekiguchi <yuki.sekiguchi@access-company.com>
2025-01-28 11:12:46 +0900

    [291344] Add GitHub Actions (#2)

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Calliope.Mixfile do
   def project do
     [ app: :calliope,
       version: "0.4.1",
-      elixir: "~> 1.3.0",
+      elixir: "~> 1.13",
       deps: [],
       package: [
         files: ["lib", "mix.exs", "README*", "LICENSE*"],
